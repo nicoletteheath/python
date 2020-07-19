e7932c98e8f24b2d047d15e3dd02ebacc8e02557
nicoletteheath <67173671+nicoletteheath@users.noreply.github.com>
2020-07-19 11:43:30 +0800

    part1 output w/o testing done

--- Project2_starter/part1/part1.py
+++ Project2_starter/part1/part1.py
@@ -65,107 +65,90 @@ def process_weather(forecast_file):
     with open(forecast_file) as json_file:
        forecast_data = json.load(json_file)
   
-    overall_minimum_temp = []
-    overall_maximum_temp = []
-    daily_summary = []
+
+
+    minimum_temps = []
+    maximum_temps = []
+    dates = []
+    day_long_phase = []
+    day_chance_rain = []
+    night_long_phase = []
+    night_chance_rain = []
     coldest_temp = None
     coldest_day = ""
     highest_temp = None
     highest_day = ""
+    output = []
 
 
-    for forecast in forecast_data["DailyForecasts"]:
-        minimum_temp = forecast["Temperature"]["Minimum"]["Value"]
-        maximum_temp = forecast["Temperature"]["Maximum"]["Value"]
-        overall_minimum_temp.append(convert_f_to_c(minimum_temp))
-        overall_maximum_temp.append(convert_f_to_c(maximum_temp))
-        date = convert_date(forecast["Date"])
-        day_long_phase = forecast["Day"]["LongPhrase"]
-        day_chance_rain = forecast["Day"]["RainProbability"]
-        night_long_phase = forecast["Night"]["LongPhrase"]
-        night_chance_rain = forecast["Night"]["RainProbability"]
+    for data in forecast_data["DailyForecasts"]:
+        minimum_temps.append(convert_f_to_c(data["Temperature"]["Minimum"]["Value"]))
+        maximum_temps.append(convert_f_to_c(data["Temperature"]["Maximum"]["Value"]))
+        dates.append(convert_date(data["Date"]))
+        day_long_phase.append(data["Day"]["LongPhrase"])
+        day_chance_rain.append(data["Day"]["RainProbability"])
+        night_long_phase.append(data["Night"]["LongPhrase"])
+        night_chance_rain.append(data["Night"]["RainProbability"])
         if coldest_temp is None:
-            coldest_temp = minimum_temp
-            coldest_day = date
-        if minimum_temp < coldest_temp:
-            coldest_temp = minimum_temp
-            coldest_day = date
+            coldest_temp = convert_f_to_c(data["Temperature"]["Minimum"]["Value"])
+            coldest_day = convert_date(data["Date"])
+        if convert_f_to_c(data["Temperature"]["Minimum"]["Value"]) < coldest_temp:
+            coldest_temp = convert_f_to_c(data["Temperature"]["Minimum"]["Value"])
+            coldest_day = convert_date(data["Date"])
         if highest_temp is None:
-            highest_temp = maximum_temp
-            highest_day = date
-        if highest_temp < maximum_temp:
-            highest_temp = maximum_temp
-            highest_day = date
-        summary_script = f"""-------- {date} -------- \nMinimum Temperature: {format_temperature(minimum_temp)}\nMaximum Temperature: {format_temperature(maximum_temp)}\nDaytime: {day_long_phase} 
-              Chance of rain: {day_chance_rain} 
-        Nightime: {night_long_phase} 
-              Chance of rain: {night_chance_rain} \n \n \n """
-        daily_summary.append(summary_script)
-   
- 
-    #return daily_summary
-
- 
+            highest_temp = convert_f_to_c(data["Temperature"]["Maximum"]["Value"])
+            highest_day = convert_date(data["Date"])
+        if convert_f_to_c(data["Temperature"]["Maximum"]["Value"]) > highest_temp:
+            highest_temp = convert_f_to_c(data["Temperature"]["Maximum"]["Value"])
+            highest_day = convert_date(data["Date"])
 
 
-    # calculating average low for 5 Day Overview
-    total_overall_minimum_temp = sum(overall_minimum_temp)
-    number_minimum_temps = len(overall_minimum_temp)
-    average_low = round(calculate_mean((total_overall_minimum_temp), (number_minimum_temps)),1)
-    # # print(average_low)
+    #calculating average low for 5 Day Overview
+    sum_of_minimum_temps = sum(minimum_temps)
+    number_minimum_temps = len(minimum_temps)
+    average_low = round(calculate_mean((sum_of_minimum_temps), (number_minimum_temps)),1)
+    # print(average_low)
 
-    # #calculating average high for 5 Day Overview
-    total_overall_maximum_temp = sum(overall_maximum_temp)
-    number_maximum_temps = len(overall_maximum_temp)
-    average_high = calculate_mean((total_overall_maximum_temp), (number_maximum_temps))
+    #calculating average high for 5 Day Overview
+    sum_of_maximum_temps = sum(maximum_temps)
+    number_of_maximum_temps = len(maximum_temps)
+    average_high = calculate_mean((sum_of_maximum_temps), (number_of_maximum_temps))
     # print(average_high)
 
 
-   
+    line = "5 Day Overview \n"
+    output.append(line)
+    line2 = f"      The lowest temperature will be {format_temperature(min(minimum_temps))}, and will occur on {coldest_day}.\n"
+    output.append(line2)
+    line3 = f"      The highest temperature will be {format_temperature(max(maximum_temps))}, and will occur on {highest_day}.\n"
+    output.append(line3)
+    line4 = f"      The average low this week is {format_temperature(average_low)}.\n"
+    output.append(line4)
+    line5 = f"      The average high this week is {format_temperature(average_high)}.\n \n \n"
+    output.append(line5)
+    # print(output)
 
-    # for each_day in forecast_data["DailyForecasts"]:
-    #     summary_script = f"""-------- {convert_date(each_day["Date"])} -------- \n
-    #     Minimum Temperature: {format_temperature(each_day["Temperature"]["Minimum"]["Value"])} \n 
-    #     Maximum Temperature: {format_temperature(each_day["Temperature"]["Maximum"]["Value"])} \n 
-    #     Daytime: {each_day["Day"]["LongPhrase"]} \n 
-    #           Chance of rain: {each_day["Day"]["RainProbability"]} \n 
-    #     Nightime: {each_day["Night"]["LongPhrase"]} \n 
-    #           Chance of rain: {each_day["Night"]["RainProbability"]} \n \n \n """
-    #     if coldest_temp is None:
-    #         coldest_temp = each_day["Temperature"]["Minimum"]["Value"]
-    #         coldest_day = convert_date(each_day["Date"])
-    #     if each_day["Temperature"]["Minimum"]["Value"] < coldest_temp:
-    #         coldest_temp = each_day["Temperature"]["Minimum"]["Value"]
-    #         coldest_day = convert_date(each_day["Date"])
-    #     if highest_temp is None:
-    #         highest_temp = each_day["Temperature"]["Maximum"]["Value"]
-    #         highest_day = convert_date(each_day["Date"])
-    #     if highest_temp < each_day["Temperature"]["Maximum"]["Value"]:
-    #         highest_temp = each_day["Temperature"]["Maximum"]["Value"]
-    #         highest_day = convert_date(each_day["Date"])
-    #     print(summary_script)
-    #     print(coldest_temp, coldest_day, highest_temp, highest_day)
+    for x in range(5):
+        dsline = f"--------{dates[x]}--------\n"
+        output.append(dsline)
+        dsline2 = f"Minimum Temperature: {format_temperature(minimum_temps[x])}\n"
+        output.append(dsline2)
+        dsline3 = f"Minimum Temperature: {format_temperature(maximum_temps[x])}\n"
+        output.append(dsline3)
+        dsline4 = f"Daytime: {day_long_phase[x]}\n"
+        output.append(dsline4)
+        dsline5 = f"    Chance of rain: {day_chance_rain[x]} % \n"
+        output.append(dsline5)
+        dsline6 = f"Nighttime: {night_long_phase[x]}\n"
+        output.append(dsline6)
+        dsline7 = f"    Chance of rain: {night_chance_rain[x]} % \n \n \n"
+        output.append(dsline7)
 
-
-
-
-    overview = f""" 5 Day Overview \n
-            The Lowest temperature will be {format_temperature(min(overall_minimum_temp))}, and will occur on {coldest_day}. \n
-            The heighest temperature will be {format_temperature(max(overall_maximum_temp))}, and will occur on {highest_day}. \n 
-            The average low this week is {format_temperature(average_low)}. \n 
-            The average high this week is {format_temperature(average_high)}. \n \n \n 
-            """
-    daily_summary.insert(0, overview)
-    final_summary = "".join(daily_summary)
-    return final_summary
-
-    # return overview, final_summary
-
-    # final_data = f"{overview} {summary_script}"
-    # return final_data
+    final_output = "".join(output)
+    return final_output
 
 
 if __name__ == "__main__":
-    print(process_weather("data/forecast_5days_a.json"))
+   print(process_weather("data/forecast_5days_a.json"))
 
  
